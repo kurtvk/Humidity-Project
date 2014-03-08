98ebe8f96afcdbc954621f2be402dd82285be33e
kurtvk <kurt.v.k@gmail.com>
2014-03-08 12:34:56 -0600

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -8,13 +8,6 @@ mounted on ladyada perm breadboard .... ....... Digole display - arduino minion 
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
 
-/*----------NOTE----------
-new version of lib will save you compiled code size for the sample
-The size for 2 version(@Arduino 1.0.1):
-      UART  I2C    SPI
-OLD   8998  8988   9132
-NEW   6966  7566   6354
-------------------------*/
 #define _Digole_Serial_UART_  //To tell compiler compile the special communication only, 
 //other available is: _Digole_Serial_I2C_ and _Digole_Serial_SPI_
 #include <DigoleSerial.h>
@@ -29,6 +22,8 @@ DigoleSerialDisp mydisp(&Serial1, 9600); //UART:Arduino Leonardo: Pin 1(TX)on ar
 #define LCDRow 2
 
 #include "DHT.h"
+#include <RF12.h>  //from jeelabs.org
+#include <Ports.h> 
 
 #define DHTPIN 2     // what pin we're connected to
 
@@ -44,7 +39,23 @@ DigoleSerialDisp mydisp(&Serial1, 9600); //UART:Arduino Leonardo: Pin 1(TX)on ar
 
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+const byte network  = 100;   // network group (can be in the range 1-255).
+const byte myNodeID = 2;     // unique node ID of receiver (1 through 30) 1-send 2-receive
+
+//Frequency of RF12B can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ.
+const byte freq = RF12_433MHZ; // Match freq to module
+
+const byte RF12_NORMAL_SENDWAIT = 0;
+
+typedef struct { int temp1, hum1, acc1, acc2; } PayloadTX;      // create structure - a neat way of packaging data for RF comms
+PayloadTX emontx;  
+
+const int emonTx_NodeID=1;            //emonTx node ID
+
+
+void setup() 
+{
+  
   Serial1.begin(9600); //start Serial1
   Serial1.println("DHTxx test!");
  
@@ -58,6 +69,10 @@ void setup() {
     //delay(1000);
     //mydisp.setLCDColRow(16,2);  //set LCD Col and Row, only time set up is OK
     mydisp.disableCursor(); //disable cursor, enable cursore use: enableCursor();
+    
+   rf12_initialize(myNodeID,freq,network);   //Initialize RFM12 with settings defined above  
+
+  
 }
 
 void loop() {
@@ -74,26 +89,63 @@ void loop() {
   } else {
     Serial.print("Humidity: "); 
     Serial.print(h);
-    Serial.print(" %\t");
+    Serial.println(" %\t");
     mydisp.print("Hum.: "); 
     mydisp.print(h);
-    mydisp.print(" %");
+    mydisp.println(" %");
     
     Serial.print("Temperature: "); 
-    Serial.print(t);
+    Serial.println(t);
     
-    mydisp.setPrintPos(0, 2);
+    mydisp.setPrintPos(0, 1);
     mydisp.print("Temp: "); 
-    mydisp.print(t);
-    mydisp.print(" C");
-    mydisp.setPrintPos(6, 3);
+    //mydisp.print(t);
+    //mydisp.print(" C");
+    mydisp.setPrintPos(6, 1);
     mydisp.print( 1.8 * t + 32);
     mydisp.print(" F");
     mydisp.setPrintPos(1, 4);
-    //mydisp.print('U+00B0');
-    //delay (1000);
-    //mydisp.drawFrame(1,60,10 ,100);
-  //delay(5000);
+    
+    mydisp.setPrintPos(0, 2);
+    mydisp.print("Remote");
+    
+    mydisp.setPrintPos(0, 3);
+    mydisp.print(emontx.hum1 /100);
+    mydisp.print(" %");
+    
+    
+    mydisp.setPrintPos(0, 4);
+    mydisp.print(1.8 * emontx.temp1/100 + 32);
+    mydisp.print(" F");
+
+    
+    
+    {
+  
+ //starts here
+ 
+ //reset temp and hum to 9999 to detect bogus info
+ //emontx.temp1 = 9999;
+ //emontx.hum1 = 9999;
+ 
+ if (rf12_recvDone()){    
+  if (rf12_crc == 0 && (rf12_hdr & RF12_HDR_CTL) == 0) {
+    
+    int node_id = (rf12_hdr & 0x1F);		  //extract nodeID from payload
+        
+    if (node_id == emonTx_NodeID)  {             //check data is coming from node with the corrct ID
+        emontx=*(PayloadTX*) rf12_data;            // Extract the data from the payload 
+       Serial.print("temp1: "); Serial.println(emontx.temp1); 
+       Serial.print("hum1: "); Serial.println(emontx.hum1); 
+       Serial.print("acc1: "); Serial.println(emontx.acc1); 
+       Serial.print("acc2: "); Serial.println(emontx.acc2); 
+       Serial.println("  "); 
+  }
+ }
+}
+}
+//ends here
+  
 
    }
 }
